b271201a0e301ea6910cfa275a772f69fb4f5c3f
Strzebinczyk <kar.strzebinczyk@gmail.com>
2025-02-28 12:46:01 +0100

    Add post author

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true
   validates :post_content, presence: true
-  validates :author_id, presence: true
+  validates :user_id, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,10 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_28_092936) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "post_content"
-    t.integer "author_id"
+    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +31,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_28_092936) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "posts", "users"
 end
